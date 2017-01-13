42eb38d1057652c9e2f39b9526f9b5768a5051ed
Whole-of-Europe <bae.gregs.base@gmail.com>
2017-01-13 21:59:29 +0000

    Initial commit

---
+++ core.py
@@ -0,0 +1,59 @@
+# License stuff
+
+import pafy
+import logging
+import Operations.audio
+
+core_logger = logging.getLogger("PlaylistDL.core")
+
+
+def var_def():
+    core_logger.debug("core.var_def() started.")
+
+    download_url = str(input("\nEnter the URL of download (can be playlist URL): "))
+    if "https://www.youtube.com/" not in download_url:
+        core_logger.error("Incorrect URL entered, must be either video URL or playlist URL from Youtube.")
+        return()
+
+    is_playlist = False
+    # Catch playlists
+    if "playlist" in download_url:
+        logging.info("Playlist URL recognised, verifying integrity.")
+        try:
+            playlist_to_dl = pafy.get_playlist(download_url)
+            print("Playlist: '{}' containing {} videos in found.".format(playlist_to_dl["title"],
+                                                                         len(playlist_to_dl["items"])))
+            is_playlist = True
+        except ValueError:
+            core_logger.error("pafy.get_playlist failed. URL entered is not found on Youtube, check for mistakes.")
+            print("Failed to find playlist, check the URL")
+            return()
+
+    if is_playlist is False:
+        try:
+            video = pafy.new(download_url)
+        except ValueError:
+            print("URL incorrect, try again.")
+            return()
+
+    download_quality = str(input("Quality of download (arbitrary High/Low): "))
+    if download_quality != "High" and download_quality != "Low":
+        print("Enter download quality correctly.")
+        return()
+    elif download_quality == "Low":
+        print("Low quality is for losers.")
+        return()
+
+    download_type = str(input("File-form to download from youtube (audio/video): "))
+    if download_type.lower() == "audio":
+        pref_audio_type = str(input("Preferred audio file (ogg/m4a): "))
+        audiodl = Operations.audio.AudioDL()
+        core_logger.debug("audioDL initialised.")
+        if is_playlist is True:
+            core_logger.debug("is_playlist = True")
+            audiodl.audio_playlist_dl(set=playlist_to_dl, fformat=pref_audio_type,
+                                      quality=download_quality)
+            core_logger.debug("Downloading function failed to start.")
+        # dl_audio(video, fformat=pref_audio_type, quality=download_quality)
+    # elif download_type.lower() == "video":
+    #     dl_video(...)
---
+++ run.py
@@ -0,0 +1,36 @@
+# license stuff
+
+import time
+import logging
+import os
+from datetime import datetime
+import core
+import Operations.audio
+
+# Setup
+print("Starting PlaylistDL.\n\nChecking for missing paths.")
+root_paths = ["./logs", "./Downloads"]
+for route_p in root_paths:
+    if os.path.exists(route_p) is False:
+        os.mkdir(route_p)
+        print("Regenerated the path, "+route_p+".")
+time.sleep(0.15)
+# Logging
+# Generate main logger
+main_logger = logging.getLogger('PlaylistDL')
+main_logger.setLevel(logging.DEBUG)
+# Generate a standard format (save line room, PEP8 and sanity)
+log_formats = logging.Formatter("{} | [%(name)s] | [%(levelname)s] : %(message)s"\
+    .format(datetime.strftime(datetime.today(), format="[%d-%m-%y | %H:%M:%S]")))
+# Set up the handler for stream
+stream_h = logging.StreamHandler()
+stream_h.setFormatter(log_formats)
+# Attach it to the main logger
+main_logger.addHandler(stream_h)
+# Set up the handler for log files
+file_h = logging.FileHandler(filename="./logs/{}.log".format(datetime.strftime(datetime.today(),
+                                                                               format="%d-%m-%y_%H%M%S")))
+file_h.setFormatter(log_formats)
+main_logger.addHandler(file_h)
+main_logger.info("Logging has been configured, if you want to change formatting you will have to alter the source.")
+core.var_def()
